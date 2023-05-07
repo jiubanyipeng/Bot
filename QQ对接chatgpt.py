edf04b9f1e1a885f45c438a981d159aabe10734b
jiubanyipeng <53213779+jiubanyipeng@users.noreply.github.com>
2023-05-07 18:26:39 +0800

    添加反向代理

--- QQ对接chatgpt.py
+++ QQ对接chatgpt.py
@@ -5,13 +5,18 @@ from requests import post as requests_post
 
 # 对接ChatGPT模型
 def generate_text(prompt):
-    global global_api_key, global_model
+    global global_api_key, global_model,global_proxy
     try:
-        # completions_endpoint = f"https://api.openai.com/v1/engines/{global_model}/completions"
-        completions_endpoint = f"https://porxy.jiubanyipeng.com/v1/engines/{global_model}/completions" # 国内无法直接访问
-        headers = { "Content-Type": "application/json","Authorization": f"Bearer {global_api_key}"}
-        data = {"prompt": prompt,"max_tokens": 1000,"temperature": 0.5}
-        response = requests_post(completions_endpoint, headers=headers, json=data)
+        if len(global_proxy) < 3:
+            completions_endpoint = f"https://api.openai.com/v1/engines/{global_model}/completions"
+        else:
+            completions_endpoint = f"{global_proxy}/{global_model}/completions"
+        headers = {"Content-Type": "application/json", "Authorization": f"Bearer {global_api_key}"}
+        data = {"prompt": prompt, "max_tokens": 1000, "temperature": 0.5}
+        try:
+            response = requests_post(completions_endpoint, headers=headers, json=data)
+        except:
+            return {"code": False, "mes": '连接不上api接口的服务器，可能是网络问题！'}
         if response.status_code == 200:
             completions = response.json()["choices"][0]["text"]  # 获取信息内容
             if response.json()["usage"]['completion_tokens'] < data.get('max_tokens'):
@@ -24,34 +29,38 @@ def generate_text(prompt):
             if response.json()['error']['code'] == 'invalid_api_key':
                 return {"code": False, "mes": 'api_key不对，请检查！'}
         if response.status_code == 400:
-            return {"code": False, "mes": '信息有误或返回太大！返回信息：'+response.json()["error"]["message"]}
+            return {"code": False, "mes": '信息有误或返回太大！返回信息：' + response.json()["error"]["message"]}
         if response.status_code == 429:
-            return {"code": False, "mes": '无法处理信息：'+response.json()["error"]["message"]}
-        return {"code": False,"mes":'连接不上api接口的服务器，网络不好！'}
+            return {"code": False, "mes": '无法处理信息：' + response.json()["error"]["message"]}
+        return {"code": False, "mes": f'api返回的信息好像有问题，信息返回：{response.text}'}
     except Exception as e:
         print(e)
-        return {"code": False,"mes":'对接ChatGPT 程序出错了！暂时不清楚情况！'}
+        return {"code": False, "mes": '对接ChatGPT 程序出错了！暂时不清楚情况！'}
 
 
 # 消息发送通用封装
-def send_msg(mes_data,user_id,type='group',group_id='0'):  # 消息是否为真、回复给谁、回复类型
+def send_msg(mes_data, user_id, type='group', group_id='0'):  # 消息是否为真、回复给谁、回复类型
     if type == 'private':
-        data_mes = {"action": "send_msg", "params": {"message_type": 'private',"user_id": user_id,'message':mes_data['mes']}}
+        data_mes = {"action": "send_msg",
+                    "params": {"message_type": 'private', "user_id": user_id, 'message': mes_data['mes']}}
     else:
-        data_mes = {"action": "send_group_msg", "params": {"group_id": group_id,"message": f"[CQ:at,qq={user_id}] {mes_data['mes']}"}}
+        data_mes = {"action": "send_group_msg",
+                    "params": {"group_id": group_id, "message": f"[CQ:at,qq={user_id}] {mes_data['mes']}"}}
 
-    return json.dumps(data_mes)   # 返回字符串形式的数据
+    return json.dumps(data_mes)  # 返回字符串形式的数据
 
 
 # 客户端接收服务端数据时触发
 def on_message(ws, message):
+    global global_show_quiz
     data = json.loads(message)
     if data.get('message_type') == 'private':  # 私人信息处理
-        message = {'code':True,'mes':f'问题：{data.get("message")} \n 正在为您查询中......'}
-        ws.send(send_msg(message, data['user_id'], 'private'))
+        if global_show_quiz == 'true':
+            message = {'code': True, 'mes': f'问题：{data.get("message")} \n 正在为您查询中......'}
+            ws.send(send_msg(message, data['user_id'], 'private'))
         # 是否仅允许部分QQ
         global global_private_disabled
-        if global_private_disabled:
+        if global_private_disabled == 'true':
             global global_private_group
             if str(data['user_id']) in global_private_group:
                 mes = generate_text(data.get('message'))  # 发送数据并返回答案,返回的是字典
@@ -67,21 +76,21 @@ def on_message(ws, message):
     elif data.get('message_type') == 'group':  # 群消息
         # 判断是否是 @ 机器人的消息
         if f"[CQ:at,qq={data['self_id']}]" in data['message']:
-            sickle_mes = data['message'].replace(f'[CQ:at,qq={data["self_id"]}]','')  # 将群信息@替换处理，仅保留信息内容
-            mes_reply  = {'code':True,'mes':f'问题：{sickle_mes} \n 正在为您查询中......'}
-            ws.send(send_msg(mes_reply,data['user_id'],group_id=data['group_id']))
+            sickle_mes = data['message'].replace(f'[CQ:at,qq={data["self_id"]}]', '')  # 将群信息@替换处理，仅保留信息内容
+            mes_reply = {'code': True, 'mes': f'正在为您查询中......'}
+            ws.send(send_msg(mes_reply, data['user_id'], group_id=data['group_id']))
             global global_group_disabled
-            if global_group_disabled:
-                mes = {'code':False,'mes':'对不起，暂时取消了对接的功能，详情请询问 玖伴一鹏'}
+            if global_group_disabled == "true":
+                mes = {'code': False, 'mes': '对不起，暂时取消了对接的功能'}
             else:
                 mes = generate_text(sickle_mes)  # 需要将数据切割再发送数据并返回答案
-            data_mes = send_msg(mes, data['user_id'],group_id=data['group_id'])  # 将信息发送到信息封装函数
+            data_mes = send_msg(mes, data['user_id'], group_id=data['group_id'])  # 将信息发送到信息封装函数
             ws.send(data_mes)
 
 
 # 通信发生错误时触发
 def on_error(ws, error):
-    print("{}返回错误:{}".format(ws,error))
+    print("{}返回错误:{}".format(ws, error))
 
 
 # 连接关闭时触发
@@ -97,10 +106,10 @@ def on_open(ws):
 # 读配置文件
 def read_setting():
     try:
-        with open('./openai_config.json','r',encoding='utf-8') as f:
-            return {'code':True,'mes':json.loads(f.read())}
+        with open('./openai_config.json', 'r', encoding='utf-8') as f:
+            return {'code': True, 'mes': json.loads(f.read())}
     except Exception as e:
-        return {'code':False,'mes':e}
+        return {'code': False, 'mes': e}
 
 
 if __name__ == "__main__":
@@ -114,15 +123,13 @@ if __name__ == "__main__":
         global_group_disabled = setting_mes['global_group_disabled']  # 默认在QQ群里面的@使用模型回答，yes则禁用，默认是全部人，不允许单个
         global_private_disabled = setting_mes['global_private_disabled']  # 是否仅允许指定的QQ号回答，yes则禁用
         global_private_group = setting_mes['global_private_group']  # 启用仅允许指定的QQ号回答，需要添加账号
+        global_proxy = setting_mes['proxy']  # 代理
+        global_show_quiz = setting_mes['show_quiz']  # 是否显示提出问题
         websocket.enableTrace(True)
-        ws = websocket.WebSocketApp(f"ws://{url}/?access_token={access_token}", on_message=on_message,on_error=on_error, on_close=on_close)
+        ws = websocket.WebSocketApp(f"ws://{url}/?access_token={access_token}", on_message=on_message,
+                                    on_error=on_error, on_close=on_close)
         ws.on_open = on_open
         ws.run_forever()
     else:
         print('配置文件有问题！')
         print(data['mes'])
-
-
-
-
-
