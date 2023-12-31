21a86576e3f91ede6b6a10bb749436a2f144fd48
jiubanyipeng <53213779+jiubanyipeng@users.noreply.github.com>
2023-12-31 18:12:40 +0800

    3.2

--- main.py
+++ main.py
@@ -7,40 +7,26 @@ from time import sleep
 
 
 def run_script(script_name):
-    subprocess.run(["python", script_name])
-
+    try:
+        subprocess.run(["python", script_name])
+    except Exception as e:
+        print(f'启动python出错：{e}')
 
 if __name__ == "__main__":
     try:
         os_makedirs('BotLog', exist_ok=True)  # 创建日志文件夹
         with open('setting_config.json', 'r', encoding='utf-8') as f:
             bot_config = json_loads(f.read())
-
-        qq_bot_run = bot_config.get('qq_bot', False)
-        weixin_run = bot_config.get('weixin_bot', False)
-
-        # 创建对接微信和对接QQ线程并启动线程
-        if qq_bot_run and weixin_run:
-            thread1 = threading.Thread(target=run_script, args=("qq_bot.py",))
-            thread2 = threading.Thread(target=run_script, args=("weixin_bot.py",))
-
-            thread1.start()
-            thread2.start()
-
-            thread1.join()
-            thread2.join()
-        elif qq_bot_run or weixin_run:
-            if qq_bot_run:
-                thread1 = threading.Thread(target=run_script, args=("qq_bot.py",))
-                thread1.start()
-                thread1.join()
-            if weixin_run:
-                thread1 = threading.Thread(target=run_script, args=("weixin_bot.py",))
-                thread1.start()
-                thread1.join()
-        else:
-            print('QQ和微信对接都选择没有启动')
-            sleep(3)
+        # 多线程，不考虑报错问题，错一个便启动不了
+        if bot_config.get('qq_bot', False):
+            qq_thread = threading.Thread(target=run_script, args=("qq_bot.py",))
+            qq_thread.start()
+        if bot_config.get('weixin_bot', False):
+            weixin_thread = threading.Thread(target=run_script, args=("weixin_bot.py",))
+            weixin_thread.start()
+        if bot_config.get('web_bot', False):
+            web_thread = threading.Thread(target=run_script, args=("web_bot.py",))
+            web_thread.start()
 
     except FileNotFoundError:
         print('对接的配置文件：setting_config 出现故障，请检查！\n如果配置文件不存在，将会创建，如果是权限不足给赋予读写文件权限。\n创建配置文件中...')
@@ -55,6 +41,9 @@ if __name__ == "__main__":
             print('远程文件无法访问，可能是本地网络问题或远程文件不存在')
             sleep(5)
     except Exception as e:
-        print('错误，请检查，以下是报错信息：\n', e)
-        print('退出...')
+        print('错误，请检查，以下是报错信息：\n', e,'\n 退出...')
         sleep(5)
+
+
+
+
